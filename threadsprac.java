eaf91ab662c240536db3498bd601f434af831303
Kamalika Saha <111375654+Saha-Kamalika@users.noreply.github.com>
2023-01-30 11:19:24 +0530

    Add files via upload

---
+++ threadsprac.java
@@ -0,0 +1,28 @@
+class MyThread1 extends Thread{
+    int i;
+    public void run() {
+        while (i < 20000) {
+            System.out.println("Thread1 class is running");
+            i++;
+        }
+    }
+}
+class MyThread2 extends Thread{
+    int i;
+    public void run(){
+        while(i<20000)
+        {
+            System.out.println("Now, Thread2 class is running");
+            i++;
+        }
+    }
+}
+public class threadsprac {
+    public static void main(String[] args)
+    {
+        MyThread1 t1=new MyThread1();
+        MyThread2 t2=new MyThread2();
+        t1.start();
+        t2.start();
+    }
+}
