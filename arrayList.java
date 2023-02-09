d47a80e144cfcb12f988c47c872e5ab380b8db93
Kamalika Saha <111375654+Saha-Kamalika@users.noreply.github.com>
2023-02-10 00:02:46 +0530

    Add files via upload

--- arrayList.java
+++ arrayList.java
@@ -1,25 +1,30 @@
-package arrayList;
+package classFiles;
 import java.util.ArrayList;
-import java.util.Set;
-import java.util.HashSet;
 public class arrayList {
     public static void main(String[] args){
-        ArrayList<Integer>al=new ArrayList<>();
+        ArrayList<Integer>al=new ArrayList();
+        ArrayList<Integer>al2=new ArrayList(5);
         al.add(10);
-        al.add(20);
+        al.add(15);
         al.add(30);
-        ArrayList<Integer>al1=new ArrayList<>();
-        al1.add(40);
-        al1.add(50);
-        System.out.println(al.remove(2));
-        System.out.println(al.contains(30));
-        System.out.println(al1.containsAll(al));
-        System.out.println(al.addAll(al1));
-        System.out.println(al.size());
-        for(Integer el:al)
-            System.out.println(el); //ordered
-        Set<Integer>s=new HashSet<>();
-        s.addAll(al);
-        System.out.println(s); //not ordered
+        al2.add(1);
+        al2.add(5);
+        al.addAll(0,al2);// adds al2 at the beginning of al
+        al.addAll(al2);// adds al2 at the end of al
+        System.out.println(al.get(2));
+        al.set(4,55);
+        for(Integer i: al)
+            System.out.print(i+" ");
+        System.out.println();
+        System.out.println(al.contains(155));
     }
 }
+/* int add(index,object)
+    boolean add(object)
+    boolean remove(object)
+    boolean addAll(Collection)
+    boolean contains(Object)
+    boolean retainsAll(Collection)
+    void set(index,Object)
+    int get(index)
+*/
