83d5efb32342fe4ac0b7e2f445578b663cc8af0d
Kamalika Saha <111375654+Saha-Kamalika@users.noreply.github.com>
2023-08-08 21:35:00 +0530

    Add files via upload

---
+++ ComparatorInterface.java
@@ -0,0 +1,22 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Collections;
+public class ComparatorInterface{
+  public static void main(String[] args){
+    List<Integer> list=new ArrayList<>();
+    list.add(110);
+    list.add(101);
+    list.add(132);
+    list.add(163);
+    //sorting the numbers based on last 2 digits 
+    Comparator<Integer> com=new Comparator<>(){
+      public int compare(Integer o1,Integer o2){
+        if(o1%100>o2%100) return 1;
+        return -1;
+      }
+    };
+    Collections.sort(list,com);
+    for(int ele:list) System.out.print(ele+" ");
+  }
+}
