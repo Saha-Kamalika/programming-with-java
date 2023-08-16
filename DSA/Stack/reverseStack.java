882b2ef24b18b17ef7bd0b9fd5c7cacad3779eed
Kamalika Saha <111375654+Saha-Kamalika@users.noreply.github.com>
2023-08-16 11:37:05 +0530

    Update reverseStack.java

--- DSA/Stack/reverseStack.java
+++ DSA/Stack/reverseStack.java
@@ -18,3 +18,36 @@ public class reverseStack {
     }
 }
 
+/*
+import java.util.*;
+public class MyStack{
+    public static void pushAtBottom(int data,Stack<Integer> s){
+        if(s.isEmpty()){
+            s.push(data);
+            return;
+        }
+        int top=s.pop();
+        pushAtBottom(data,s);
+        s.push(top);
+    }
+    public static void reverse(Stack<Integer> s){
+        if(s.isEmpty()){
+            return;
+        }
+        int top=s.pop();
+        reverse(s);
+        pushAtBottom(top,s);
+    }
+    public static void main(String[] args){
+        Stack<Integer> s=new Stack<>();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        
+        reverse(s);
+        System.out.println("Reversed Stack: ");
+        while(!s.isEmpty()){
+            System.out.print(s.pop());
+        }
+    }
+}*/
