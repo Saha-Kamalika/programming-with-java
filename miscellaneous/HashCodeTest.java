cb5d50a4f52e3de4c878b4bb56663cff6bcebdb2
Kamalika Saha <111375654+Saha-Kamalika@users.noreply.github.com>
2023-08-17 07:52:06 +0530

    Create HashCodeTest.java

---
+++ miscellaneous/HashCodeTest.java
@@ -0,0 +1,12 @@
+public class Test{
+    public static void main(String[] args){
+        String s="kama";
+        System.out.println(s.hashCode());
+        s=s+"lika";
+        System.out.println(s.hashCode());
+        StringBuilder sb=new StringBuilder("kama");
+        System.out.println(sb.hashCode());
+        sb.append("lika");
+        System.out.println(sb.hashCode());
+    }
+}
