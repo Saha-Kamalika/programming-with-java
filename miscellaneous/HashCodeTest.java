e47a0ffae058241054ac9dd1cc1ab4b1fc85e043
Kamalika Saha <111375654+Saha-Kamalika@users.noreply.github.com>
2023-08-19 21:04:34 +0530

    Update HashCodeTest.java

--- miscellaneous/HashCodeTest.java
+++ miscellaneous/HashCodeTest.java
@@ -1,5 +1,5 @@
 public class Test{
-    public static void main(String[] args){
+    public static void main(String[] args) {
         String s="kama";
         System.out.println(s.hashCode());
         s=s+"lika";
