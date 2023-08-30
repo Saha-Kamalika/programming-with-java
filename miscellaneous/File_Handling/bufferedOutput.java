b8f516fa73d928cdd6d2bd9a70d6b6835bc6c1dd
Kamalika Saha <111375654+Saha-Kamalika@users.noreply.github.com>
2023-08-30 22:13:04 +0530

    Update bufferedOutput.java

--- miscellaneous/File_Handling/bufferedOutput.java
+++ miscellaneous/File_Handling/bufferedOutput.java
@@ -2,7 +2,7 @@ package com.files;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
-public class bufferedOutput {
+public class bufferedOutput  {
 	public static void main(String[] args) throws IOException{
 		FileWriter f=new FileWriter("C:\\\\Users\\\\kamal\\\\eclipse-workspace\\\\FileHandling\\\\src\\\\com\\\\files\\\\myfile.txt",true);
 		BufferedWriter b=new BufferedWriter(f);
