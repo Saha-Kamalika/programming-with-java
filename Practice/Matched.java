d95276d40fbe911de90553a747259ba0a6dd3c49
Kamalika Saha <111375654+Saha-Kamalika@users.noreply.github.com>
2023-08-31 19:26:18 +0530

    Add files via upload

---
+++ Practice/Matched.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+import java.util.Set;
+public class Matched {
+public static void main(String[] args) {
+	HashMap<Character,Integer> hm=new HashMap<>();
+	String str="kamalika";
+	for(int i=0;i<str.length();i++) {
+		char c=str.charAt(i);
+		if(hm.containsKey(c)) {
+			int count=hm.get(c);
+			hm.put(c,++count);
+		}else hm.put(c,1);
+	}
+	Set<Character> chars=hm.keySet();
+	for(Character ch: chars) {
+		int c=hm.get(ch);
+		if(c>1) System.out.println(ch+" "+c);
+	}
+}
+}
