620b101aa53154cedf82c8d7dd0238a99566e526
Kamalika Saha <111375654+Saha-Kamalika@users.noreply.github.com>
2024-03-29 10:18:58 +0530

    Add files via upload

---
+++ medicineByPrice.java
@@ -0,0 +1,73 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class medicineByPrice {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        Medicine[] medicines = new Medicine[4];
+        for(int i=0;i<4;i++){
+            medicines[i]=new Medicine(sc.next(), sc.next(), sc.next(), sc.nextInt());
+        }
+        String disease=sc.next();
+        Integer[] ans= getPriceByDisease(medicines, disease);
+        for(int i=0;i<ans.length;i++){
+            System.out.println(ans[i]);
+        }
+    }
+    public static Integer[] getPriceByDisease(Medicine[] medicines, String disease){
+        Integer[] ans=new Integer[0];
+        for(int i=0;i<medicines.length;i++){
+            if(medicines[i].getDisease().equalsIgnoreCase(disease)){
+                ans= Arrays.copyOf(ans, ans.length+1);
+                ans[ans.length-1]= medicines[i].getPrice();
+            }
+        }
+        Arrays.sort(ans);
+        return ans;
+    }
+}
+class Medicine{
+    private String MedicineName;
+    private String batch;
+    private String disease;
+    private int price;
+
+    public String getMedicineName() {
+        return MedicineName;
+    }
+
+    public void setMedicineName(String medicineName) {
+        MedicineName = medicineName;
+    }
+
+    public String getBatch() {
+        return batch;
+    }
+
+    public void setBatch(String batch) {
+        this.batch = batch;
+    }
+
+    public String getDisease() {
+        return disease;
+    }
+
+    public void setDisease(String disease) {
+        this.disease = disease;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public Medicine(String medicineName, String batch, String disease, int price) {
+        MedicineName = medicineName;
+        this.batch = batch;
+        this.disease = disease;
+        this.price = price;
+    }
+}
---
+++ uniqueChars.java
@@ -0,0 +1,18 @@
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Scanner;
+
+public class uniqueChars {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.next();
+        LinkedHashSet<Character> hs=new LinkedHashSet<>();
+        char[] ch=s.toCharArray();
+        for(Character c: ch){
+            hs.add(c);
+        }
+        for(Character c: hs){
+            System.out.print(c);
+        }
+    }
+}
